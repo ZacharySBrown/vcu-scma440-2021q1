6d7ef1dd6de641e6171f6e458a28d17eb9ab959c
zak <zachary.s.brown@gmail.com>
2021-01-05 17:35:45 -0800

    initial commit with first example

---
+++ data/examples/fetch_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/beer.dat > beer.dat
+curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/shampoo.dat > shampoo.dat
+
