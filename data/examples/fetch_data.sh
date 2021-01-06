0b91775897979fab37e579f4801096f0995327d1
zak <zachary.s.brown@gmail.com>
2021-01-06 10:59:57 -0800

    fetch_data.sh

--- data/examples/fetch_data.sh
+++ data/examples/fetch_data.sh
@@ -2,4 +2,4 @@
 
 curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/beer.dat > beer.dat
 curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/shampoo.dat > shampoo.dat
-
+curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/japmpg.dat > jampg.dat
