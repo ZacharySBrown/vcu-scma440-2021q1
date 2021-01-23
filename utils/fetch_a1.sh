2704388217d71ca0d4284309e68ec4ecb2aeefea
zak.brown <zachary.s.brown@gmail.com>
2021-01-23 17:37:45 -0500

    data

---
+++ utils/fetch_a1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# fetch data
+curl https://vcu-scma-440.s3.amazonaws.com/data/legacy/02/actuals.csv > actuals.csv
+curl https://vcu-scma-440.s3.amazonaws.com/data/legacy/02/historical.csv > historical.csv
+
+
+
+
+
+
--- utils/fetch_examples.sh
+++ utils/fetch_examples.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Spyros Data
 curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/beer.dat > beer.dat
 curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/shampoo.dat > shampoo.dat
 curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/japmpg.dat > jampg.dat
+
+# Supplemental
+curl https://vcu-dapt-615.s3.amazonaws.com/covid_19_data.csv > covid.csv
+curl https://vcu-dapt-615.s3.amazonaws.com/properties_2016.csv.zip > housing.csv
