4fbe7762193736878dda42d750c42351e05412cc
zak.brown <zachary.s.brown@gmail.com>
2021-01-25 17:34:25 -0500

    updated beer data

--- utils/fetch_e1.sh
+++ utils/fetch_e1.sh
@@ -4,7 +4,7 @@
 curl https://vcu-scma-440.s3.amazonaws.com/data/ames_housing.csv > ames_housing.csv
 
 # Beer
-curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/beer.dat > beer.dat
+curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/beer2.dat > beer2.dat
 
 # MPG
 curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/japmpg.dat > jampg.dat
