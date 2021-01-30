7188578487176cec794cb323b084471101e24996
zak.brown <zachary.s.brown@gmail.com>
2021-01-30 15:54:48 -0500

    a1p1

--- utils/fetch_a1.sh
+++ utils/fetch_a1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Assignment 1
-curl https://vcu-scma-440.s3.amazonaws.com/data/a1_beer.csv > a1_beer.csv
+curl https://vcu-scma-440.s3.amazonaws.com/data/a1_beer_time_series.csv > a1_beer_time_series.csv
+curl https://vcu-scma-440.s3.amazonaws.com/data/a1_beer_data.csv > a1_beer_data.csv
