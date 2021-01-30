8597ecba5bcfa01f46de2600a695a705ac42bb15
zak.brown <zachary.s.brown@gmail.com>
2021-01-30 17:04:28 -0500

    japanese auto data

--- utils/fetch_a1.sh
+++ utils/fetch_a1.sh
@@ -3,3 +3,4 @@
 # Assignment 1
 curl https://vcu-scma-440.s3.amazonaws.com/data/a1_beer_time_series.csv > a1_beer_time_series.csv
 curl https://vcu-scma-440.s3.amazonaws.com/data/a1_beer_data.csv > a1_beer_data.csv
+curl https://raw.githubusercontent.com/FinYang/tsdl/56e091544cb81e573ee6db20c6f9cd39c70e6243/data-raw/data/japcars.dat > japcars.dat
