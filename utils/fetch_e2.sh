7713fe152d5f16e6ff1ae3685f09ee392b2d3bc8
zak.brown <zachary.s.brown@gmail.com>
2021-02-02 12:04:21 -0500

    fetch examples script for chapter 3

---
+++ utils/fetch_e2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Shampoo
+curl https://vcu-scma-440.s3.amazonaws.com/data/shampoo_sales.csv > shampoo_sales.csv
+
+# Housing
+curl https://vcu-scma-440.s3.amazonaws.com/data/housing_sales.csv > housing_sales.csv
