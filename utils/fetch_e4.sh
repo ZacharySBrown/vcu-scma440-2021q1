50bcf9d31cadac27f29baea9d6eae8fe9901eee2
zak.brown <zachary.s.brown@gmail.com>
2021-02-15 09:36:59 -0500

    added fetch

---
+++ utils/fetch_e4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Shampoo
+curl https://vcu-scma-440.s3.amazonaws.com/data/shampoo_sales.csv > shampoo_sales.csv
+
+# Housing
+curl https://vcu-scma-440.s3.amazonaws.com/data/housing_sales.csv > housing_sales.csv
+
+# Airlines
+curl https://vcu-scma-440.s3.amazonaws.com/data/airline.csv > airline.csv
+curl https://vcu-scma-440.s3.amazonaws.com/data/airline_series.csv > airline_series.csv
+
+# Can Openers
+curl https://vcu-scma-440.s3.amazonaws.com/data/can_openers.csv > can_openers.csv
